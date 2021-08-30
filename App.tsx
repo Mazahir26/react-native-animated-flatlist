7852b21c3303a4434220544439f488a73062f04c
Mazahir26 <supermazahir@gmail.com>
2021-08-30 11:22:39 +0530

    added react-native-paper  --npm

--- App.tsx
+++ App.tsx
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+import faker from 'faker';
+
+faker.seed(26);
+
+interface data  {
+  name: string,
+  email: string,
+  job_title: string,
+  key: string,
+  avatar: string,
+}
 
 export default function App() {
+  const fake : string[] = new Array(20).fill('test');
+  const data: data[] = fake.map(() => ({
+    name: faker.name.findName(),
+    job_title: faker.name.jobTitle(),
+    email: faker.internet.email(),
+    key: faker.random.alphaNumeric(10),
+    avatar: faker.image.avatar(),
+  }));
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>List</Text>
     </View>
   );
 }
@@ -14,8 +35,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
   },
 });
---
+++ components/list.tsx
@@ -0,0 +1,85 @@
+import * as React from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Animated,
+  Dimensions,
+} from "react-native";
+import { Avatar, Surface } from "react-native-paper";
+
+const { height, width } = Dimensions.get("screen");
+
+interface Data {
+  name: string;
+  email: string;
+  job_title: string;
+  key: string;
+  avatar: string;
+}
+
+export default function List({ data } : {data: Data[]}) {
+  const scrollY = React.useRef(new Animated.Value(0)).current;
+
+  return (
+    <View style={styles.container}>
+      <Animated.FlatList
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+          { useNativeDriver: true }
+        )}
+        data={data}
+        keyExtractor={(item) => item.key}
+        renderItem={({ item, index }) => {
+          const inputRange = [
+            -1,
+            0,
+            height * 0.1 * index,
+            height * 0.1 * (index + 2),
+          ];
+          const scale = scrollY.interpolate({
+            inputRange,
+            outputRange: [1, 1, 1, 0],
+          });
+          return (
+            <Animated.View style={{ transform: [{ scale: scale }] }}>
+              <Surface style={styles.surface}>
+                <View style={{ flex: 0.3, justifyContent: "center" }}>
+                  <Avatar.Image size={42} source={{ uri: item.avatar }} />
+                </View>
+                <View
+                  style={{
+                    flex: 0.7,
+                    flexDirection: "column",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Text style={{ fontSize: 22, fontWeight: "bold" }}>
+                    {item.name}
+                  </Text>
+                  <Text style={{ fontSize: 14 }}>{item.job_title}</Text>
+                </View>
+              </Surface>
+            </Animated.View>
+          );
+        }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  surface: {
+    height: height * 0.1,
+    marginTop: 15,
+    padding: 8,
+    elevation: 4,
+    marginHorizontal: 10,
+    borderRadius: 8,
+    flexDirection: "row",
+  },
+});
